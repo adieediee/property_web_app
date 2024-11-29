b0a635ad9ab61ff648b3cd3e3f8767044c1ae464
AdrianaGemelova <gemelova1@stud.uniza.sk>
2024-11-29 16:26:54 +0100

    Pridanie expandovania karticiek v casti Poruchy

--- PropertyWebApp/Models/Property.cs
+++ PropertyWebApp/Models/Property.cs
@@ -6,9 +6,22 @@ namespace PropertyWebApp.Models
     {
         [Key]
         public int PropertyId { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public decimal MonthlyRent { get; set; }
         public bool IsAvailable { get; set; }
+        public string PropertyName { get; set; }
+        public int TypeId { get; set; }
+        public DateTime ListingDate { get; set; }
+        public string Description { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public decimal Price { get; set; }
+        public int Area { get; set; }
+        public short NumberOfBedrooms { get; set; }
+        public short NumberOfBathrooms { get; set; }
+        public bool IsFurnished { get; set; }
+        public bool ParkingAvailable { get; set; }
+
     }
 }
---
+++ PropertyWebApp/Models/Services/PropertyService.cs
@@ -0,0 +1,29 @@
+﻿namespace PropertyWebApp.Models.Services
+{
+    public class PropertyService
+    {
+        private readonly List<Property> _properties = new();
+
+        public Task<List<Property>> GetPropertiesAsync()
+        {
+            return Task.FromResult(_properties);
+        }
+
+        public Task AddPropertyAsync(Property property)
+        {
+            _properties.Add(property);
+            return Task.CompletedTask;
+        }
+
+        public Task DeletePropertyAsync(int propertyId)
+        {
+            var property = _properties.FirstOrDefault(p => p.PropertyId == propertyId);
+            if (property != null)
+            {
+                _properties.Remove(property);
+            }
+            return Task.CompletedTask;
+        }
+    }
+
+}
